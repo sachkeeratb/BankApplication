51b316e321c92e7bf0285105aa0a0a9727f73f31
sachkeeratb <sachkeeratbrar@gmail.com>
2024-05-22 10:16:34 -0400

    title

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void title() {
+    // This method outputs the title of the application and the creators of it
+    System.out.println("""
+        ___            _       _             _ _         _   _         \s
+       | _ ) __ _ _ _ | |__   /_\\  _ __ _ __| (_)__ __ _| |_(_)___ _ _ \s
+       | _ \\/ _` | ' \\| / /  / _ \\| '_ \\ '_ \\ | / _/ _` |  _| / _ \\ ' \\\s
+       |___/\\__,_|_||_|_\\_\\ /_/ \\_\\ .__/ .__/_|_\\__\\__,_|\\__|_\\___/_||_|
+                                  |_|  |_|                             \s
+      
+      By: Sachkeerat Brar, Nimay Desai, and Kushal Prajapati
+      """);
+  }
+  public static void main(String[] args) {
+    title();
+  }
 }
