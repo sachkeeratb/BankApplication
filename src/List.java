0ea53606480130c3cd54c5e56fd0d4270582ea07
Nimay <robtop.test.gd@gmail.com>
2024-05-24 09:45:27 -0400

    Add User

---
+++ src/List.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+// Nimasy Desai
+public class List {
+    private Node head;
+
+    class Node {
+        User info;
+        Node link;
+        public Node(User i, Node l) {
+            info = i;
+            link = l;
+        }
+    }
+
+
+
+}
